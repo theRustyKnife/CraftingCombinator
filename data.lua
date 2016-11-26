786e386c0e2281aefe212d621983fec2defd9add
theRustyKnife <therustyknife13@gmail.com>
2016-11-26 21:37:29 +0100

    Started a complete rewrite
    * Changed name - no idea about backwards compatibility yet...
    * Moved special cases and icons to the locale mod
    + Added support for external special case and icon definitions
    
    No control yet, just creating virtual recipes

---
+++ data.lua
@@ -0,0 +1,37 @@
+-- define entities here, only name and slot ite_slot_count needed, rest is from constant-combinator
+local entities = {
+	{
+		name = "crafting-combinator",
+		item_slot_count = 0,
+	},
+	{
+		name = "recipe-combinator",
+		item_slot_count = 20,
+	}
+}
+
+-- we create our entities with all the necessary stuff too
+for _, e in ipairs(entities) do
+	-- entity
+	local te = util.table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
+	te.name = e.name
+	te.minable.result = e.name
+	te.item_slot_count = e.item_slot_count
+	
+	-- item
+	local ti = util.table.deepcopy(data.raw["item"]["constant-combinator"])
+	ti.name = e.name
+	ti.place_result = e.name
+	ti.order = "b[combinators]-cb[crafting-combinator]"
+	
+	-- recipe
+	local tr = util.table.deepcopy(data.raw["recipe"]["constant-combinator"])
+	tr.name = e.name
+	tr.result = e.name
+	
+	-- add to data
+	data:extend{te, ti, tr}
+	
+	-- tech
+	table.insert(data.raw.technology["circuit-network"].effects, {type = "unlock-recipe", recipe = e.name})
+end
