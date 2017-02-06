679a4c842499303dd10db88f1d55e52890dd7c8a
Nexela <nexela@gmail.com>
2017-02-06 03:03:48 -0500

    Virtual Signals, Dynamic Locales, Better Icon retrieval

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -18,27 +18,74 @@ local function needs_signal(recipe) -- string recipe
 -- does it?
 	local name = recipe
 	recipe = data.raw.recipe[name]
-	
+
 	local res = not is_result(recipe, name)                    -- no need for a signal if there is an item with the same name
 	res = res and not data.raw["virtual-signal"][name]         -- no need for a signal if there already is a signal with that name
 	res = res and not crafting_combinator_data.overrides[name] -- I TRUST U DO DA RITE TING
-	
+
 	return res
 end
 
 local function get_icon(recipe) -- LuaRecipe recipe
 -- attempts to find the best suitable icon for this recipe - uses a default one if none is found
 	if crafting_combinator_data.icons[recipe.name] then return crafting_combinator_data.icons[recipe.name]; end                                -- an icon is explicitly defined
-	if recipe.icon then return recipe.icon; end                                                                                                -- the recipe has it's icon
-	if recipe.result and data.raw.item[recipe.result] and data.raw.item[recipe.result].icon then return data.raw.item[recipe.result].icon; end -- the result has an icon
-	if recipe.results then
-		for _, v in pairs(recipe.results) do
-			if v.name == recipe.name and data.raw.item[v.name] and data.raw.item[v.name].icon then return data.raw.item[v.name].icon; end      -- there's a matching item in the results table with an icon
+	if recipe.icon then return {{icon = recipe.icon}}; end                                                                                                -- the recipe has it's icon
+	if recipe.icons then return recipe.icons; end
+	for _, type in pairs({"item", "module", "tool", "fluid"}) do
+		if recipe.result and data.raw[type][recipe.result] and data.raw[type][recipe.result].icon then return {{icon = data.raw[type][recipe.result].icon}}; end -- the result has an icon
+		if recipe.result and data.raw[type][recipe.result] and data.raw[type][recipe.result].icons then return data.raw[type][recipe.result].icons; end
+		if recipe.results then
+			for _, v in pairs(recipe.results) do
+				if v.name == recipe.name and data.raw[type][v.name] and data.raw[type][v.name].icon then
+					return {{data.raw[type][v.name].icon}};
+				elseif v.name == recipe.name and data.raw[type][v.name] and data.raw[type][v.name].icons then
+					return data.raw[type][v.name].icons
+				end      -- there's a matching item in the results table with an icon
+			end
 		end
 	end
-	return "__crafting_combinator__/graphics/no-icon.png"                                                                                      -- no icon found - use the default one
+	return {{icon = "__crafting_combinator__/graphics/no-icon.png"}}-- no icon found - use the default one
 end
 
+local function get_locale(recipe)
+	--Try the best option to get a valid localised name
+
+	local cur_type, item, result_item
+	for _, type in pairs({"item", "module", "tool", "fluid"}) do
+	cur_type = type
+	item = data.raw[type][recipe.name]
+	result_item = data.raw[type][recipe.result] or (recipe.results and data.raw[type][recipe.results[1].name])
+	if item or result_item then break end
+	end
+
+	local loc_key = {"recipe-name."..recipe.name}
+	if recipe.localised_name then
+		loc_key = recipe.localised_name
+	elseif item and item.localised_name then
+		loc_key = item.localised_name
+	elseif result_item and result_item.type == "fluid" then
+		loc_key = {"fluid-name."..result_item.name}
+	elseif result_item then
+		loc_key = {"item-name."..result_item.name}
+	elseif item and item.place_result then
+		loc_key = {"entity-name."..item.place_result}
+	elseif item and recipe.placed_as_equipment_result then
+		loc_key = {"equipment-name."..item.placed_as_equipment_result}
+	end
+	return loc_key
+end
+
+local ignore_name_list = {
+	-- "angels%-fluid%-splitter-",
+	-- "converter%-angels%-",
+	-- "compress%-",
+	-- "uncompress%-",
+}
+local function ignore_recipes(name)
+	for _, ignore_name in ipairs(ignore_name_list) do
+		if name:find("^"..ignore_name) then return true end
+	end
+end
 
 -- create the subgroup for virtual recipes
 data:extend{
@@ -53,12 +100,13 @@ data:extend{
 -- create virtual recipes
 for name, recipe in pairs(data.raw.recipe) do
 	-- only make virtual recipes if the name is not the same as result (or one of the results)
-	if needs_signal(name) then
+	if needs_signal(name) and not (recipe.hidden or ignore_recipes(recipe.name)) then
 		data:extend{
 			{
 				type = "virtual-signal",
 				name = name,
-				icon = get_icon(recipe),
+				localised_name = get_locale(recipe),
+				icons = get_icon(recipe),
 				subgroup = "virtual-signal-recipe",
 				order = "zzz[virtual-signal-recipe]" .. (recipe.order or ""),
 			}
