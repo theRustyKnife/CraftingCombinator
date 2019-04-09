84b20079575016223018b87f904097f7d636d934
TheRustyKnife <therustyknife13@gmail.com>
2019-04-09 15:52:01 +0200

    Uhm, made changes?

--- crafting_combinator/data.lua
+++ crafting_combinator/data.lua
@@ -1,142 +1,179 @@
-local FML = require "FML.init"
-local config = require "config"
+local config = require 'config'
 
 
 -- Crafting Combinator
-local cc = FML.data.make_prototype{
-	base = FML.data.inherit("constant-combinator"),
-	properties = {
-		name = config.CC_NAME,
-		icon = "__crafting_combinator__/graphics/icon-crafting-combinator.png",
-		icon_size = 32,
-		item_slot_count = 3,
-	},
-	auto_generate = {
-		item = {properties = {subgroup = "circuit-network"}},
-		recipe = {base = FML.data.inherit("recipe", "constant-combinator"), unlock_with = "circuit-network"},
-	},
-}
--- change images
+local cc = table.deepcopy(data.raw['constant-combinator']['constant-combinator'])
+cc.name = config.CC_NAME
+cc.icon = '__crafting_combinator__/graphics/icon-crafting-combinator.png'
+cc.icon_size = 32
+cc.item_slot_count = 3
+cc.minable.result = cc.name
+table.insert(cc.flags, 'not-deconstructable')
+
 for _, image in pairs(cc.sprites) do
-	image.filename = "__crafting_combinator__/graphics/crafting-combinator-entities.png"
-	image.y = 0
+	local im = image.layers[1]
+	im.filename = '__crafting_combinator__/graphics/entities.png'
+	im.y = 0
+	im.hr_version = nil
 end
 
--- Recipe Combinator
-local rc = FML.data.make_prototype{
-	base = FML.data.inherit("constant-combinator"),
-	properties = {
-		name = config.RC_NAME,
-		icon = "__crafting_combinator__/graphics/icon-recipe-combinator.png",
-		icon_size = 32,
-		item_slot_count = config.RC_SLOT_COUNT,
+local cc_item = table.deepcopy(data.raw['item']['constant-combinator'])
+cc_item.name = cc.name
+cc_item.place_result = cc.name
+cc_item.icon = cc.icon
+cc_item.subgroup = 'circuit-network'
+cc_item.order = 'c[combinators]-m[crafting-combinator]'
+
+local cc_recipe = table.deepcopy(data.raw['recipe']['constant-combinator'])
+cc_recipe.name = cc.name
+cc_recipe.result = cc.name
+table.insert(data.raw['technology']['circuit-network'].effects, {type = 'unlock-recipe', recipe = cc.name})
+
+
+local rc = table.deepcopy(data.raw['arithmetic-combinator']['arithmetic-combinator'])
+rc.name = config.RC_NAME
+rc.minable.result = rc.name
+rc.energy_source = { type = 'void' }
+rc.energy_usage_per_tick = '1W'
+--TODO: Graphics
+
+local rc_item = table.deepcopy(data.raw['item']['arithmetic-combinator'])
+rc_item.name = rc.name
+rc_item.place_result = rc.name
+rc_item.icon = rc.icon
+rc_item.subgroup = 'circuit-network'
+rc_item.order = 'c[combinators]-m[recipe-combinator]'
+
+local rc_recipe = table.deepcopy(data.raw['recipe']['arithmetic-combinator'])
+rc_recipe.name = rc.name
+rc_recipe.result = rc.name
+table.insert(data.raw['technology']['circuit-network'].effects, {type = 'unlock-recipe', recipe = rc.name})
+
+
+local trans = {
+	filename = '__crafting_combinator__/graphics/trans.png',
+	width = 1,
+	height = 1,
+}
+local con_point = {
+	wire = {
+		red = {0, 0},
+		green = {0, 0},
 	},
-	auto_generate = {
-		item = {properties = {subgroup = "circuit-network"}},
-		recipe = {base = FML.data.inherit("recipe", "constant-combinator"), unlock_with = "circuit-network"},
+	shadow = {
+		red = {0, 0},
+		green = {0, 0},
 	},
 }
--- change images
-for _, image in pairs(rc.sprites) do
-	image.filename = "__crafting_combinator__/graphics/crafting-combinator-entities.png"
-	image.y = 63
-end
 
 
-FML.data.make_prototypes{
+data:extend {
+	cc, cc_item, cc_recipe,
+	rc, rc_item, rc_recipe,
 	{
-		type = "item-group",
+		type = 'item',
+		name = config.MODULE_CHEST_NAME,
+		flags = {'hidden'},
+		stack_size = 1,
+		place_result = config.MODULE_CHEST_NAME,
+		icon = cc.icon,
+		icon_size = cc.icon_size,
+	},
+	{
+		type = 'container',
+		name = config.MODULE_CHEST_NAME,
+		flags = {'placeable-off-grid', 'not-blueprintable', 'not-upgradable', 'player-creation'},
+		collision_mask = {},
+		collision_box = cc.collision_box,
+		selection_box = cc.selection_box,
+		inventory_size = config.MODULE_CHEST_SIZE,
+		picture = trans,
+		minable = {mining_time = 0.2, result = cc.name},
+	},
+	{
+		type = 'item',
+		name = config.SETTINGS_ENTITY_NAME,
+		flags = {'hidden'},
+		stack_size = 1,
+		place_result = config.SETTINGS_ENTITY_NAME,
+		icon = '__base__/graphics/icons/blueprint.png',
+		icon_size = 32,
+	},
+	{
+		type = 'programmable-speaker',
+		name = config.SETTINGS_ENTITY_NAME,
+		flags = {'placeable-off-grid', 'placeable-neutral', 'player-creation', 'not-deconstructable', 'not-upgradable'},
+		collision_mask = {},
+		collision_box = cc.collision_box,
+		selection_box = {{0, 0}, {0, 0}},
+		sprite = trans,
+		icon = '__base__/graphics/icons/blueprint.png',
+		icon_size = 32,
+		
+		minable = {
+			result = config.SETTINGS_ENTITY_NAME,
+			mining_time = 0,
+		},
+		
+		energy_source = { type = 'void' },
+		energy_usage_per_tick = '1W',
+		
+		instruments = {},
+		maximum_polyphony = 0,
+	},
+	{
+		type = 'constant-combinator',
+		name = config.RC_PROXY_NAME,
+		flags = {'placeable-off-grid'},
+		collision_mask = {},
+		item_slot_count = config.RC_SLOT_COUNT,
+		circuit_wire_max_distance = 3,
+		sprites = {
+			north = trans,
+			east = trans,
+			south = trans,
+			west = trans,
+		},
+		activity_led_sprites = trans,
+		activity_led_light_offsets = {{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+		
+		circuit_wire_connection_points = {con_point, con_point, con_point, con_point},
+		draw_circuit_wires = false,
+	},
+	
+	{
+		type = 'item-group',
 		name = config.GROUP_NAME,
-		order = "fb",
-		icon = "__crafting_combinator__/graphics/recipe-book.png",
+		order = 'fb',
+		icon = '__crafting_combinator__/graphics/recipe-book.png',
 		icon_size = 64,
 	},
 	{
-		type = "item-subgroup",
-		name = "crafting_combinator-signals",
+		type = 'item-subgroup',
+		name = 'crafting_combinator:signals',
 		group = config.GROUP_NAME,
-		order = "___",
+		order = '___',
 	},
 	{
-		type = "item-subgroup",
-		name = config.RECIPE_SUBGROUP_NAME,
+		type = 'item-subgroup',
+		name = config.UNSORTED_RECIPE_SUBGROUP,
 		group = config.GROUP_NAME,
-		order = "zzz[unsorted]",
+		order = 'zzz[unsorted]',
 	},
 	{
-		type = "virtual-signal",
-		name = config.TIME_NAME,
-		icon = "__core__/graphics/clock-icon.png",
-		subgroup = "crafting_combinator-signals",
-		order = "a[recipe-time]",
+		type = 'virtual-signal',
+		name = config.TIME_SIGNAL_NAME,
+		icon = '__core__/graphics/clock-icon.png',
+		subgroup = 'crafting_combinator:signals',
+		order = 'a[recipe-time]',
 		icon_size = 32,
 	},
 	{
-		type = "virtual-signal",
-		name = config.SPEED_NAME,
-		icon = "__crafting_combinator__/graphics/speed-icon.png",
-		subgroup = "crafting_combinator-signals",
-		order = "b[crafting-speed]",
+		type = 'virtual-signal',
+		name = config.SPEED_SIGNAL_NAME,
+		icon = '__crafting_combinator__/graphics/speed-icon.png',
+		subgroup = 'crafting_combinator:signals',
+		order = 'b[crafting-speed]',
 		icon_size = 32,
-	},
-	{ -- the active overflow chest
-		base = FML.data.inherit("logistic-container", "logistic-chest-active-provider"),
-		properties = {
-			name = config.OVERFLOW_A_NAME,
-			render_not_in_network_icon = false,
-			flags = {"placeable-off-grid"},
-			minable = nil,
-			selectable_in_game = false,
-			selection_box = nil,
-			collision_mask = {},
-			hidden = true,
-			item_slot_count = config.OVERFLOW_SLOT_COUNT,
-			picture = {
-				filename = "__crafting_combinator__/graphics/trans.png",
-				width = 1,
-				height = 1,
-			},
-			circuit_wire_max_distance = 0,
-		},
-	},
-	{ -- the regular overflow chest
-		base = FML.data.inherit("container", "steel-chest"),
-		properties = {
-			name = config.OVERFLOW_N_NAME,
-			flags = {"placeable-off-grid"},
-			minable = nil,
-			selectable_in_game = false,
-			selection_box = nil,
-			collision_mask = {},
-			hidden = true,
-			item_slot_count = config.OVERFLOW_SLOT_COUNT,
-			picture = {
-				filename = "__crafting_combinator__/graphics/trans.png",
-				width = 1,
-				height = 1,
-			},
-			circuit_wire_max_distance = 0,
-		},
-	},
-	{
-		type = "custom-input",
-		name = config.MENU_KEY_NAME,
-		key_sequence = "mouse-button-1",
-		consuming = "none",
-	},
-	{
-		type = "custom-input",
-		name = config.CLOSE_KEY_NAME,
-		key_sequence = "E",
-		consuming = "none",
-	},
-}
-
-
-FML.data.make_prototype{ -- the passive overflow chest
-	base = FML.data.inherit("logistic-container", config.OVERFLOW_A_NAME),
-	properties = {
-		name = config.OVERFLOW_P_NAME,
-		logistic_mode = "passive-provider",
 	},
 }
---
+++ crafting_combinator/script/recipe-selector.lua
@@ -0,0 +1,71 @@
+local _M = {}
+
+
+function _M.get_recipe(entity, items_to_ignore, connector_id)
+	items_to_ignore = items_to_ignore or {}
+	local signals
+	if connector_id ~= nil then signals = entity.get_merged_signals(connector_id)
+	else signals = entity.get_merged_signals(); end
+	if not signals then return nil; end
+	
+	local res = nil
+	local count = nil
+	for _, signal in pairs(signals) do
+		local recipe = entity.force.recipes[signal.signal.name]
+		if recipe and recipe.enabled then
+			local c = signal.count - (items_to_ignore[recipe.name] or 0)
+			if count == nil or c > count then res = recipe; count = c; end
+		end
+	end
+	
+	return res, count
+end
+
+
+function _M.get_highest_signal(signals)
+	local res = nil
+	local count = nil
+	
+	for _, signal in pairs(signals) do
+		local c = signal.count
+		if count == nil or c > count then res, count = signal.signal.name, c; end
+	end
+	
+	return res, count or 0
+end
+
+function _M.get_recipes(signals, recipes)
+	if not signals then return {}, 0; end
+	local highest, count = _M.get_highest_signal(signals)
+	if not highest then return {}, 0; end
+	
+	local res = {}
+	local item
+	if game.item_prototypes[highest] then item = {name = highest, type = 'item'}
+	elseif game.fluid_prototypes[highest] then item = {name = highest, type = 'fluid'}
+	else item = {}; end
+	
+	for name, recipe in pairs(recipes) do
+		if not recipe.hidden and recipe.enabled then
+			for _, product in pairs(recipe.products) do
+				if product.name == item.name and (item.type == 'fluid' or item.type == product.type) then
+					table.insert(res, name)
+					break
+				end
+			end
+		end
+	end
+	
+	return res, count
+end
+
+
+function _M.get_signal(recipe)
+	return {
+		name = recipe,
+		type = (game.item_prototypes[recipe] and 'item') or (game.fluid_prototypes[recipe] and 'fluid') or 'virtual'
+	}
+end
+
+
+return _M
