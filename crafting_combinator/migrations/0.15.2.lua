50947395a3cf9d21cbe7d9ee9b9ed48fa263fff7
TheRustyKnife <therustyknife13@gmail.com>
2020-03-27 18:52:27 +0100

    Drop existing caches

---
+++ crafting_combinator/migrations/0.15.2.lua
@@ -0,0 +1,7 @@
+late_migrations['0.15.2'] = function()
+	local signals = require 'script.signals'
+	
+	for _, cache in pairs(global.signals.cache) do signals.cache.drop(cache.__entity); end
+	global.signals = nil
+	signals.init_global()
+end
